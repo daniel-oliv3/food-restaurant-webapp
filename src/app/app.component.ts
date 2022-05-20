38c9205538d140c7c49a4fa8465f877b294a5686
Daniel Oliveira <39778932+daniel-oliv3@users.noreply.github.com>
2022-05-20 16:47:35 -0300

    Create angular project

---
+++ src/app/app.component.ts
@@ -0,0 +1,28 @@
+import { Component } from '@angular/core';
+
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+
+
+export class AppComponent {
+
+  //...
+  lista_nomes: string[] = [
+    'Daniel', 'Priscila', 'Jojob'
+  ];
+
+  }
+
+
+
+
+
+
+
+
+
+
